33c8f9706599219e68dc231181379b5dc991253b
Animesh Deshpande <animeshdeshpande@Animeshs-MacBook-Pro.local>
2022-04-28 21:38:58 -0700

    Database starting, postgres

--- myapp/vendor/github.com/Animesh-M-Deshpande/celeritas/celeritas.go
+++ myapp/vendor/github.com/Animesh-M-Deshpande/celeritas/celeritas.go
@@ -30,6 +30,7 @@ type Celeritas struct {
 	Routes   *chi.Mux
 	Render   *render.Render
 	Session  *scs.SessionManager
+	DB       Database
 	JetViews *jet.Set
 	config   config
 }
@@ -39,6 +40,7 @@ type config struct {
 	renderer    string
 	cookie      cookieConfig
 	sessionType string
+	database    databaseConfig
 }
 
 // New reads the .env file, creates our application config, populates the Celeritas type with settings
@@ -67,6 +69,23 @@ func (c *Celeritas) New(rootPath string) error {
 
 	// create loggers
 	infoLog, errorLog := c.startLoggers()
+
+	// connect to database
+	if os.Getenv("DATABASE_TYPE") != "" {
+
+		db, err := c.OpenDB(os.Getenv("DATABASE_TYPE"), c.BuildDSN())
+
+		if err != nil {
+			errorLog.Println(err)
+			os.Exit(1)
+		}
+
+		c.DB = Database{
+			DataType: os.Getenv("DATABASE_TYPE"),
+			Pool:     db,
+		}
+	}
+
 	c.InfoLog = infoLog
 	c.ErrorLog = errorLog
 	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
@@ -164,4 +183,29 @@ func (c *Celeritas) createRenderer() {
 		JetViews: c.JetViews,
 	}
 	c.Render = &myRenderer
+}
+
+func (c *Celeritas) BuildDSN() string {
+	var dsn string
+
+	fmt.Println("connection string: databaseType:", os.Getenv("DATABASE_TYPE"))
+	switch os.Getenv("DATABASE_TYPE") {
+	case "postgres", "postgressql":
+		dsn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s timezone=UTC connect_timeout=5",
+			os.Getenv("DATABASE_HOST"),
+			os.Getenv("DATABASE_PORT"),
+			os.Getenv("DATABASE_USER"),
+			os.Getenv("DATABASE_NAME"),
+			os.Getenv("DATABASE_SSL_MODE"))
+
+		if os.Getenv("DATABASE_PASS") != "" {
+			dsn = fmt.Sprintf("%s password=%s", dsn, os.Getenv("DATABASE_PASS"))
+		}
+		fmt.Println("connection string:2:", dsn)
+	default:
+
+	}
+
+	fmt.Println("connection string:", dsn)
+	return dsn
 }
