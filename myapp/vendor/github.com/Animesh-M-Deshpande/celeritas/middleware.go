6d50ea9b83fa998ce61165a554db1d5b16b6dd21
Animesh Deshpande <animeshdeshpande@Animeshs-MacBook-Pro.local>
2022-05-08 00:30:09 -0700

    Removed ignored files from index

--- myapp/vendor/github.com/Animesh-M-Deshpande/celeritas/middleware.go
+++
@@ -1,9 +0,0 @@
-package celeritas
-
-import "net/http"
-
-func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
-
-	c.InfoLog.Println("session load called")
-	return c.Session.LoadAndSave(next)
-}
