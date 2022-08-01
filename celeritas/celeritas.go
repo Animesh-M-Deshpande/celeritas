c4735d86b43ffeae87c7a61b30829eb5e4673555
Animesh Deshpande <animeshdeshpande@Animeshs-MacBook-Pro.local>
2022-07-31 19:44:24 -0700

    Lecture:86: Testing redis cache

--- celeritas/celeritas.go
+++ celeritas/celeritas.go
@@ -8,11 +8,13 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/Animesh-M-Deshpande/celeritas/cache"
 	"github.com/Animesh-M-Deshpande/celeritas/render"
 	"github.com/Animesh-M-Deshpande/celeritas/session"
 	"github.com/CloudyKit/jet/v6"
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
+	"github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
 )
 
@@ -34,6 +36,7 @@ type Celeritas struct {
 	JetViews      *jet.Set
 	config        config
 	EncryptionKey string
+	Cache         cache.Cache
 }
 
 type config struct {
@@ -42,6 +45,7 @@ type config struct {
 	cookie      cookieConfig
 	sessionType string
 	database    databaseConfig
+	redis       redisConfig
 }
 
 // New reads the .env file, creates our application config, populates the Celeritas type with settings
@@ -87,6 +91,11 @@ func (c *Celeritas) New(rootPath string) error {
 		}
 	}
 
+	// check if the cache setup is empty
+	if os.Getenv("CACHE") == "redis" {
+		myRedisCache := c.createClientRedisCache()
+		c.Cache = myRedisCache
+	}
 	c.InfoLog = infoLog
 	c.ErrorLog = errorLog
 	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
@@ -108,6 +117,11 @@ func (c *Celeritas) New(rootPath string) error {
 		database: databaseConfig{
 			database: os.Getenv("DATABASE_TYPE"),
 			dsn:      c.BuildDSN(),
+		},
+		redis: redisConfig{
+			host:     os.Getenv("REDIS_HOST"),
+			password: os.Getenv("REDIS_PASSWORD"),
+			prefix:   os.Getenv("REDIS_PREFIX"),
 		},
 	}
 
@@ -217,4 +231,27 @@ func (c *Celeritas) BuildDSN() string {
 
 	fmt.Println("connection string:", dsn)
 	return dsn
+}
+
+func (c *Celeritas) createClientRedisCache() *cache.RedisCache {
+	cacheClient := cache.RedisCache{
+		Conn:   c.createRedisPool(),
+		Prefix: c.config.redis.prefix,
+	}
+	return &cacheClient
+}
+
+func (c *Celeritas) createRedisPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     50,
+		MaxActive:   10000,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp:", c.config.redis.host, redis.DialPassword(c.config.redis.password))
+		},
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			_, err := conn.Do("PING")
+			return err
+		},
+	}
 }
